2955ae7a0111f23e9803c2aee2efe15e40949685
AmerAbuZaiton123 <amerabuzaiton@outlook.com>
2021-10-02 20:02:55 +0300

    fix deploy

--- src/component/Weather2.js
+++ src/component/Weather2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card , Button} from 'react-bootstrap'
+import  Card  from 'react-bootstrap'
 export default class Weather2 extends Component {
     render() {
         return (
