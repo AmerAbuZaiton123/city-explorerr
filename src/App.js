b85f1975fafeca9d92a81a03a2892906feeeaddb
AmerAbuZaiton123 <amerabuzaiton@outlook.com>
2021-10-02 19:57:47 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import FormS from "./component/FormS"
-// import Error from "./component/Error"
-// import Location from "./component/Location"
+import Error from "./component/Error"
+import Location from "./component/Location"
 export default class App extends Component {
  
   render() {
